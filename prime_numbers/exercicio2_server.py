9389747ea2fda539d406b397c59bd15ff9d59c9c
Victor Freitas <victorfreitasking@gmail.com>
2024-02-27 23:00:06 -0300

    results part 1 and fixes update

--- prime_numbers/exercicio2_server.py
+++ prime_numbers/exercicio2_server.py
@@ -2,15 +2,30 @@ from socket import socket, AF_INET, SOCK_STREAM
 from threading import Thread
 import time, pickle
 
-max = 100
-num_threads = 2
+max = 1000
+num_threads = 4
 connections = []
 count_connections = 0
 
 result = []
 
+
+def split_range(start, end):
+    size = (end - start) / num_threads
+    parts = []
+
+    for i in range(num_threads):
+        part = [
+            int(start + size * i),
+            int(start + size * (i + 1))
+        ]
+        parts.append(part)
+        print(part)
+
+    return parts
+
 def broadcast_parts(nums):
-    for i in range(len(nums)):
+    for i in range(0, num_threads):
         conn, addr = connections[i]
         conn.send(pickle.dumps(nums[i]))
         parcial_results = pickle.loads(conn.recv(4096))
@@ -21,26 +36,6 @@ def start():
     global count_connections, num_threads
     server_socket = socket(AF_INET, SOCK_STREAM)
 
-    nums = []
-    i=0
-    while(i<max+1):
-        nums.append(int(i+1))
-        i+=int(max/num_threads)
-    nums[len(nums)-1]=max
-
-    start_indexes = []
-    end_indexes = []
-    start_indexes.append(nums.pop(0))
-    end = nums.pop()
-    for num in nums:
-        start_indexes.append(num)
-        end_indexes.append(num)
-
-    end_indexes.append(end)
-
-    indexes = []
-    indexes.append(start_indexes)
-    indexes.append(end_indexes)
 
     print('Servidor iniciado!\n')
 
@@ -57,7 +52,7 @@ def start():
 
             if count_connections == num_threads:
                 print('Conexões  atingidas!\n')
-                broadcast_parts(indexes)
+                broadcast_parts(split_range(1, max))
                 break
         print(result)
 
